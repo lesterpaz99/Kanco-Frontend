f64537a3872379bedd8b77c91646968f17ebb0a7
Obed Paz <lesterpaz99@gmail.com>
2023-09-10 14:34:25 -0600

    Update next config file for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				// port: 'localhost:3000',
+				// pathname: '',
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
