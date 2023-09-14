b23f2ce3e347fb5763c6353dd4eb5afd0c6250bc
Obed Paz <62860279+lesterpaz99@users.noreply.github.com>
2023-09-14 16:13:16 -0600

    Setup theme colors tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
@@ -13,6 +14,33 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          light: '#deded2',
+          DEFAULT: '#21212d',
+          dark: '#21212d'
+        },
+        secondary: {
+          light: '#d3d3c7',
+          DEFAULT: '#2c2c38',
+          dark: '#2c2c38'
+        },
+        cta: {
+          light: '#9ba039',
+          DEFAULT: '#645fc6',
+          dark: '#645fc6'
+        },
+        text: {
+          light: '#7b725e',
+          DEFAULT: '#848da1',
+          dark: '#848da1'
+        },
+        headings: {
+          light: '#000',
+          DEFAULT: '#fff',
+          dark: '#fff'
+        },
+      }
     },
   },
   plugins: [],
